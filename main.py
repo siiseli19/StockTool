5f301bd41b729e3bfdba9f1a007b5e030bd43a56
Sami <sami.ojala97@gmail.com>
2024-02-08 22:50:33 +0200

    update

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 
 import requests
+from bs4 import BeautifulSoup
+import re
+import pandas as pd
+import time
+import seaborn as sns
+
+
+#user agent   headers = {'User-Agent': "your@email.com"}
+user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36'
 
 
 
